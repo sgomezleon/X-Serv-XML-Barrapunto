62614ddf0a0e7afb63d00d586c7ad5552673626f
sgomezleon <sergom19@hotmail.com>
2017-04-23 01:39:53 +0200

    barrapunto titulares

---
+++ xml-parser-barrapunto.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#
+# Simple XML parser for the RSS channel from BarraPunto
+# Jesus M. Gonzalez-Barahona
+# jgb @ gsyc.es
+# TSAI and SAT subjects (Universidad Rey Juan Carlos)
+# September 2009
+#
+# Just prints the news (and urls) in BarraPunto.com,
+#  after reading the corresponding RSS channel.
+
+from xml.sax.handler import ContentHandler
+from xml.sax import make_parser
+import sys
+
+class myContentHandler(ContentHandler):
+
+    body = ""
+
+    def __init__ (self):
+        self.inItem = False
+        self.inContent = False
+        self.theContent = ""
+
+    def startElement (self, name, attrs):
+        if name == 'item':
+            self.inItem = True
+        elif self.inItem:
+            if name == 'title':
+                self.inContent = True
+            elif name == 'link':
+                self.inContent = True
+            
+    def endElement (self, name):
+        if name == 'item':
+            self.inItem = False
+        elif self.inItem:
+            if name == 'title':
+                line = "Title: " + self.theContent + "."
+                # To avoid Unicode trouble
+                print line.encode('utf-8') 
+                self.body = self.body + line
+                self.inContent = False
+                self.theContent = ""
+            elif name == 'link':
+                print " Link: " + self.theContent + "."
+                link = "<a href=" + self.theContent + "> Enlace: " + self.theContent + "</a>"
+                self.body = self.body + link
+                self.inContent = False
+                self.theContent = ""
+
+    def characters (self, chars):
+        if self.inContent:
+            self.theContent = self.theContent + chars
+            
+# --- Main prog
+
+if len(sys.argv)<2:
+    print "Usage: python xml-parser-barrapunto.py <document>"
+    print
+    print " <document>: file name of the document to parse"
+    sys.exit(1)
+    
+# Load parser and driver
+
+theParser = make_parser()
+theHandler = myContentHandler()
+theParser.setContentHandler(theHandler)
+
+# Ready, set, go!
+
+xmlFile = open(sys.argv[1],"r")
+theParser.parse(xmlFile)
+theHandler.body = theHandler.body 
+
+print "Parse complete"
+#print theHandler.body
+
